6831cc79a98324de5cfdf762b725ea53c95df1c0
Brendan Ross <brendan.ross@accenture.com>
2020-11-02 13:00:06 -0600

    remove useless functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,14 @@
 /* src/App.js */
-import React, { useEffect, useState, CSSProperties } from "react";
-import Amplify, { API, graphqlOperation } from "aws-amplify";
-import { listTodos } from "./graphql/queries";
+import React, { CSSProperties } from "react";
+import Amplify from "aws-amplify";
 
 import CreateTodo from "./components/CreateTodo";
 import TodoList from "./components/TodosList";
-
-import TodoConnection from "./types/TodoConnection";
-import Todo from "./types/Todo";
 
 import awsExports from "./aws-exports";
 Amplify.configure(awsExports);
 
 const App = () => {
-	const [todos, setTodos] = useState<Todo[]>([]);
-
-	useEffect(() => {
-		fetchTodos();
-	}, []);
-
-	async function fetchTodos() {
-		try {
-			const todoData = (await API.graphql(
-				graphqlOperation(listTodos)
-			)) as TodoConnection;
-
-			const todos = todoData.data.listTodos.items;
-			setTodos(todos);
-		} catch (err) {
-			console.log("error fetching todos");
-		}
-	}
-
 	return (
 		<div style={styles.container}>
 			<h2>Amplify Todos</h2>
